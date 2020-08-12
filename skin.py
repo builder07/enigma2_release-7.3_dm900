24c8e90870c2ee4d2a54d863e77b73a11deabf62
builder07 <47144637+builder07@users.noreply.github.com>
2020-08-12 23:22:56 +0300

    Update skin.py

--- skin.py
+++ skin.py
@@ -75,15 +75,14 @@ def skin_user_skinname():
 # the dom_skin will keep all screens in descending priority,
 # so the first screen found will be used.
 
-
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = SystemInfo["HasFullHDSkinSupport"] and "PLi-FullNightHD/skin.xml" or "PLi-HD/skin.xml"
+DEFAULT_SKIN = SystemInfo["HasFullHDSkinSupport"] and "PLi-FullNightHD-FIX/skin.xml" or "PLi-HD-FIX/skin.xml"
 ### DEFAULT_SKIN = "PLi-HD/skin.xml"
 # on SD hardware, PLi-HD will not be available
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
 	# in that case, fallback to Magic (which is an SD skin)
-	DEFAULT_SKIN = "Magic/skin.xml"
+	DEFAULT_SKIN = "PLi-FullNightHD-FIX/skin.xml"
 	if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
 		DEFAULT_SKIN = "skin.xml"
 config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)
