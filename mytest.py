eed8902821b359698e6c06fdc5b66eaedf24c33d
builder07 <47144637+builder07@users.noreply.github.com>
2020-10-04 21:53:22 +0300

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -568,24 +568,6 @@ profile("LCD")
 import Components.Lcd
 Components.Lcd.InitLcd()
 
-from boxbranding import getBoxType
-boxtype = getBoxType()
-if boxtype in ('dm7080', 'dm820', 'dm900', 'dm920', 'gb7252'):
-	f=open("/proc/stb/hdmi-rx/0/hdmi_rx_monitor","r")
-	check=f.read()
-	f.close()
-	if check.startswith("on"):
-		f=open("/proc/stb/hdmi-rx/0/hdmi_rx_monitor","w")
-		f.write("off")
-		f.close()
-	f=open("/proc/stb/audio/hdmi_rx_monitor","r")
-	check=f.read()
-	f.close()
-	if check.startswith("on"):
-		f=open("/proc/stb/audio/hdmi_rx_monitor","w")
-		f.write("off")
-		f.close()
-
 profile("RFMod")
 import Components.RFmod
 Components.RFmod.InitRFmod()
